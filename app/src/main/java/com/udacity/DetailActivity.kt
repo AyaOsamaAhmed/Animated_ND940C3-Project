fbbcaf74342be1dc865190ffc857cedc760a63a4
Aya Osama Ahmed <37303559+AyaOsamaAhmed@users.noreply.github.com>
2022-08-30 23:01:03 +0200

    notification , motion

--- app/src/main/java/com/udacity/DetailActivity.kt
+++ app/src/main/java/com/udacity/DetailActivity.kt
@@ -3,13 +3,91 @@ package com.udacity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_detail.*
+import androidx.annotation.ColorRes
+import androidx.annotation.DrawableRes
+import androidx.core.content.ContextCompat
+import androidx.core.os.bundleOf
+import androidx.databinding.DataBindingUtil
+import com.udacity.BuildConfig
+import com.udacity.R
+import com.udacity.DownloadStatus
+import com.udacity.DownloadStatus.FAILED
+import com.udacity.DownloadStatus.SUCCESSFUL
+import com.udacity.databinding.ActivityDetailBinding
+
 
 class DetailActivity : AppCompatActivity() {
 
+    /*private val fileName by lazy {
+        intent?.extras?.getString(EXTRA_FILE_NAME, unknownText) ?: unknownText
+    }
+    private val downloadStatus by lazy {
+        intent?.extras?.getString(EXTRA_DOWNLOAD_STATUS, unknownText) ?: unknownText
+    }
+*/
+    private val unknownText by lazy { getString(R.string.unknown) }
+
+    private lateinit var binding : ActivityDetailBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail)
-        setSupportActionBar(toolbar)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_detail)
+
+        binding.apply {
+                setSupportActionBar(toolbar)
+                content_detail.initializeView()
+            }
+
     }
+
+
+    private fun DetailContentBinding.initializeView() {
+        fileNameText.text = fileName
+        downloadStatusText.text = downloadStatus
+        okButton.setOnClickListener { finish() }
+        changeViewForDownloadStatus()
+    }
+
+    private fun DetailContentBinding.changeViewForDownloadStatus() {
+        when (downloadStatusText.text) {
+            SUCCESSFUL.statusText -> {
+                changeDownloadStatusImageTo(R.drawable.ic_check_circle_outline_24)
+                changeDownloadStatusColorTo(R.color.colorPrimaryDark)
+            }
+            FAILED.statusText -> {
+                changeDownloadStatusImageTo(R.drawable.ic_error_24)
+                changeDownloadStatusColorTo(R.color.design_default_color_error)
+            }
+        }
+    }
+
+    private fun DetailContentBinding.changeDownloadStatusImageTo(@DrawableRes imageRes: Int) {
+        downloadStatusImage.setImageResource(imageRes)
+    }
+
+    private fun DetailContentBinding.changeDownloadStatusColorTo(@ColorRes colorRes: Int) {
+        ContextCompat.getColor(this@DetailActivity, colorRes)
+            .also { color ->
+                downloadStatusImage.imageTintList = ColorStateList.valueOf(color)
+                downloadStatusText.setTextColor(color)
+            }
+    }
+
+    companion object {
+        private const val EXTRA_FILE_NAME = "${BuildConfig.APPLICATION_ID}.FILE_NAME"
+        private const val EXTRA_DOWNLOAD_STATUS = "${BuildConfig.APPLICATION_ID}.DOWNLOAD_STATUS"
+
+        /**
+         * Creates a [Bundle] with given parameters and pass as data to [DetailActivity].
+         */
+        fun bundleExtrasOf(
+            fileName: String,
+            downloadStatus: DownloadStatus
+        ) = bundleOf(
+            EXTRA_FILE_NAME to fileName,
+            EXTRA_DOWNLOAD_STATUS to downloadStatus.statusText
+        )
+    }
+
 
 }
--- app/src/main/java/com/udacity/LoadingButton.kt
+++ app/src/main/java/com/udacity/LoadingButton.kt
@@ -3,16 +3,14 @@ package com.udacity
 import android.animation.AnimatorSet
 import android.animation.ValueAnimator
 import android.content.Context
-import android.graphics.Canvas
-import android.graphics.Paint
-import android.graphics.RectF
-import android.graphics.Typeface
+import android.graphics.*
 import android.text.TextPaint
 import android.util.AttributeSet
 import android.view.View
 import android.view.animation.LinearInterpolator
 import androidx.core.content.ContextCompat
 import androidx.core.content.withStyledAttributes
+import timber.log.Timber
 import java.lang.Math.min
 import java.util.concurrent.TimeUnit
 import kotlin.properties.Delegates
@@ -25,6 +23,9 @@ class LoadingButton @JvmOverloads constructor(
     private var widthSize = 0
     private var heightSize = 0
     private var currentProgressCircleAnimationValue = 0f
+
+    // It'll be initialized when first Loading state is trigger
+    private lateinit var buttonTextBounds: Rect
 
     // region Progress Circle/Arc variables
     private val progressCircleRect = RectF()
@@ -66,12 +67,59 @@ class LoadingButton @JvmOverloads constructor(
     // region Animation variables
     private val animatorSet: AnimatorSet = AnimatorSet().apply {
         duration = THREE_SECONDS
-      //  disableViewDuringAnimation(this@LoadingButton)
+        disableViewDuringAnimation(this@LoadingButton)
     }
 
-    private var buttonState: ButtonState by Delegates.observable<ButtonState>(ButtonState.Completed) { p, old, new ->
+    private var buttonState: ButtonState by Delegates.observable<ButtonState>(ButtonState.Completed) { p, old, newState  ->
 
+        Timber.d("Button state changed: $newState")
+        when (newState) {
+            ButtonState.Loading -> {
+                // LoadingButton is now Loading and we need to set the correct text
+                buttonText = loadingText.toString()
+
+                // We only calculate ButtonText bounds and ProgressCircle rect once,
+                // Only when buttonText is first initialized with loadingText
+                if (!::buttonTextBounds.isInitialized) {
+                    retrieveButtonTextBounds()
+                    computeProgressCircleRect()
+                }
+
+                // ProgressCircle and Button background animations must start now
+                animatorSet.start()
+            }
+            else -> {
+                // LoadingButton is not doing any Loading so we need to reset to default text
+                buttonText = loadingDefaultText.toString()
+
+                // ProgressCircle animation must stop now
+                newState.takeIf { it == ButtonState.Completed }?.run { animatorSet.cancel() }
+            }
+        }
     }
+
+    private fun computeProgressCircleRect() {
+        val horizontalCenter =
+            (buttonTextBounds.right + buttonTextBounds.width() + PROGRESS_CIRCLE_LEFT_MARGIN_OFFSET)
+        val verticalCenter = (heightSize / BY_HALF)
+
+        progressCircleRect.set(
+            horizontalCenter - progressCircleSize,
+            verticalCenter - progressCircleSize,
+            horizontalCenter + progressCircleSize,
+            verticalCenter + progressCircleSize
+        )
+    }
+
+
+    /**
+     * Initialize and retrieve the text boundary box of [buttonText] and store it into [buttonTextBounds].
+     */
+    private fun retrieveButtonTextBounds() {
+        buttonTextBounds = Rect()
+        buttonTextPaint.getTextBounds(buttonText, 0, buttonText.length, buttonTextBounds)
+    }
+
 
 
     init {
@@ -137,7 +185,7 @@ class LoadingButton @JvmOverloads constructor(
         }
     }
 
-    private fun Canvas.drawButtonText() {
+    private fun drawButtonText() {
         // Draw the Loading Text at the Center of the Canvas
         // ref.: https://blog.danlew.net/2013/10/03/centering_single_line_text_in_a_canvas/
         buttonTextPaint.color = loadingTextColor
@@ -155,7 +203,7 @@ class LoadingButton @JvmOverloads constructor(
     /**
      * Draws the default button background color using [loadingBackgroundColor].
      */
-    private fun Canvas.drawBackgroundColor() {
+    private fun drawBackgroundColor() {
         when (buttonState) {
             ButtonState.Loading -> {
                 drawLoadingBackgroundColor()
@@ -168,7 +216,7 @@ class LoadingButton @JvmOverloads constructor(
     /**
      * Draws the [Rect] with [loadingBackgroundColor] representing the loading progress.
      */
-    private fun Canvas.drawLoadingBackgroundColor() = buttonPaint.apply {
+    private fun drawLoadingBackgroundColor() = buttonPaint.apply {
         color = loadingBackgroundColor
     }.run {
         drawRect(
@@ -183,7 +231,7 @@ class LoadingButton @JvmOverloads constructor(
     /**
      * Draws the [Rect] with [loadingDefaultBackgroundColor] representing the background of the button.
      */
-    private fun Canvas.drawDefaultBackgroundColor() = buttonPaint.apply {
+    private fun drawDefaultBackgroundColor() = buttonPaint.apply {
         color = loadingDefaultBackgroundColor
     }.run {
         drawRect(
@@ -198,7 +246,7 @@ class LoadingButton @JvmOverloads constructor(
     /**
      * Draws progress circle if [buttonState] is [ButtonState.Loading].
      */
-    private fun Canvas.drawProgressCircleIfLoading() =
+    private fun drawProgressCircleIfLoading() =
         buttonState.takeIf { it == ButtonState.Loading }?.let { drawProgressCircle(this) }
 
     /**
@@ -206,7 +254,7 @@ class LoadingButton @JvmOverloads constructor(
      * The sweep angle uses [currentProgressCircleAnimationValue] which is changed according to when
      * [progressCircleAnimator] send updates after the values for the animation have been calculated.
      */
-    private fun drawProgressCircle(buttonCanvas: Canvas) {
+    private fun drawProgressCircle(buttonCanvas: LoadingButton) {
         buttonPaint.color = progressCircleBackgroundColor
         buttonCanvas.drawArc(
             progressCircleRect,
@@ -231,6 +279,17 @@ class LoadingButton @JvmOverloads constructor(
         setMeasuredDimension(w, h)
     }
 
+
+    /**
+     * Changes [buttonState] to the given [state] if they are not the same causing the view to be
+     * redrawn.
+     */
+    fun changeButtonState(state: ButtonState) {
+        if (state != buttonState) {
+            buttonState = state
+            invalidate()
+        }
+    }
 
     // region LoadingButton constants
     companion object {
